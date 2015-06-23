e7c0e22fd383eee5f11cca253c0e9969dea4ceff
Brian McCallister <brianm@skife.org>
2015-06-22 17:51:22 -0700

    playign with first api;

--- ferries/client.go
+++ ferries/client.go
@@ -1,5 +1,13 @@
 package ferries
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const BaseUrl = "http://www.wsdot.wa.gov/ferries/api/schedule/rest"
+
 type Client struct {
 	apiKey string
 }
@@ -8,4 +16,19 @@ func NewClient(apiKey string) Client {
 	return Client{
 		apiKey: apiKey,
 	}
+}
+
+type Terminal struct {
+	Description string
+	TerminalId  int
+}
+
+func (c Client) Terminals(time.Time) (error, []Terminal) {
+
+	url := fmt.Sprintf("%s/terminals/%d-%d-%d?apiaccesstoken=%s", BaseUrl, 2015, 6, 22, c.apiKey)
+	_, err := http.Get(url)
+	if err != nil {
+		return err, []Terminal{}
+	}
+	return nil, []Terminal{}
 }
