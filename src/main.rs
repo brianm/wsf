5ffcf861d9d89642e63809be067a43dce58bdc64
Brian McCallister <brianm@skife.org>
2015-07-08 17:09:28 -0700

    lookup arrive and dest by prefix

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,74 @@ extern crate rustc_serialize;
 extern crate chrono;
 extern crate regex;
 
+use std::ascii::AsciiExt;
 use std::io::Read;
 use std::env;
 
-use chrono::*;
+use chrono::naive::datetime::NaiveDateTime;
+use chrono::offset::local::Local;
+use chrono::offset::fixed::FixedOffset;
+use chrono::datetime::DateTime;
+use chrono::Datelike;
+
 use rustc_serialize::json;
 use hyper::client::Client;
 use regex::Regex;
 
+fn main() {
+    let mut args      = env::args();
+    let _program      = args.next().expect("program should be first arg!");
+    let from_in: &str = &args.next().expect("must specify from");
+    let to_in: &str   = &args.next().expect("must specify to");
+
+    // pull in api key at *build* time from environment
+    let s = Session::new(env!("WSDOT_API_KEY"));
+
+    let now = Local::now();
+    let mut from: Option<i32> = None;
+    let mut to: Option<i32> = None;
+    let terminals = s.terminals();
+    for terminal in terminals.iter() {
+        if terminal.Description.to_ascii_lowercase().starts_with(&from_in) {
+            from = Some(terminal.TerminalID);
+        }
+        if terminal.Description.to_ascii_lowercase().starts_with(&to_in) {
+            to = Some(terminal.TerminalID);
+        }
+    }
+
+    let url = &s.url(format!("/schedule/{}-{}-{}/{}/{}",
+                             now.year(), now.month(), now.day(), from.unwrap(), to.unwrap()));
+
+    let mut res = s.client.get(url).send().unwrap();
+    assert_eq!(res.status, hyper::Ok);
+
+    let mut buf = String::new();
+    res.read_to_string(&mut buf).unwrap();
+    let schedule: ScheduleResult = json::decode(&buf).unwrap();
+
+    assert_eq!(1, schedule.TerminalCombos.len());
+    let tc = &schedule.TerminalCombos[0];
+    for time in tc.Times.iter() {
+        if time.depart_time() > now {
+            println!("{}\t{}\t{}\t{}",
+            time.depart_time().time(), tc.DepartingTerminalName, tc.ArrivingTerminalName, time.VesselName );
+        }
+
+    }
+}
+
 struct Session {
-    api_key: &'static str,
+    api_key: String,
+    client: Client,
 }
 
 impl Session {
-    fn new(api_key: &'static str) -> Session {
-        Session { api_key: api_key }
+    fn new(api_key: &str) -> Session {
+        Session {
+            api_key: api_key.to_string(),
+            client: Client::new(),
+        }
     }
 
     fn url(&self, path: String) -> String {
@@ -25,92 +78,74 @@ impl Session {
                 path,
                 self.api_key)
     }
-}
 
-#[allow(non_snake_case)]
-#[derive(RustcDecodable, Debug)]
-struct SailingTime {
-    DepartingTime: String,
-    ArrivingTime: Option<String>
-}
+    fn terminals(&self) -> Vec<Terminal> {
+        let now = Local::today();
+        let url = &self.url(format!("/terminals/{}-{}-{}", now.year(), now.month(), now.day()));
 
+        let mut res = self.client.get(url).send().unwrap();
+        assert_eq!(res.status, hyper::Ok);
 
-impl SailingTime {
-    // parse strings of form "/Date(1436318400000-0700)/"
-    fn depart_time(&self, ltz: Local) -> DateTime<Local> {
-        let re = Regex::new(r"^/Date\((\d{10})000-(\d{2})(\d{2})\)/$").unwrap();
-        let caps = re.captures(&self.DepartingTime).unwrap();
-
-        let epoch_s = caps.at(1).unwrap();
-        let epoch: i64 =  epoch_s.parse().unwrap();
-
-        let tzhs = caps.at(2).unwrap();
-        let tzhi: i32 = tzhs.parse().unwrap();
-
-        let tzms = caps.at(3).unwrap();
-        let tzmi: i32 = tzms.parse().unwrap();
-
-        let nd = NaiveDateTime::from_timestamp(epoch, 0);
-        let tz = FixedOffset::west((tzhi * 3600) + (tzmi * 60));
-        let fotz: DateTime<FixedOffset> = DateTime::from_utc(nd, tz);
-        fotz.with_timezone(&ltz)
+        let mut buf = String::new();
+        res.read_to_string(&mut buf).unwrap();
+        let routes: Vec<Terminal> = json::decode(&buf).unwrap();
+        routes
     }
 }
 
-
 #[allow(non_snake_case)]
-#[derive(RustcDecodable, Debug)]
-struct TerminalCombo {
-    Times: Vec<SailingTime>,
+#[derive(RustcDecodable, RustcEncodable, Debug, Clone)]
+struct Terminal {
+    TerminalID: i32,
+    Description: String,
 }
 
 #[allow(non_snake_case)]
-#[derive(RustcDecodable, Debug)]
+#[derive(RustcDecodable, RustcEncodable, Debug)]
+struct SailingTime {
+    DepartingTime: String,
+    ArrivingTime: Option<String>,
+    VesselName: String,
+}
+
+impl SailingTime {
+    // parse date strings of form "/Date(1436318400000-0700)/"
+    fn depart_time(&self) -> DateTime<Local> {
+        let re = Regex::new(r"^/Date\((\d{10})000-(\d{2})(\d{2})\)/$").unwrap();
+        let caps = re.captures(&self.DepartingTime).unwrap();
+
+        let epoch: i64 = caps.at(1).unwrap().parse().unwrap();
+        let tz_hours: i32 = caps.at(2).unwrap().parse().unwrap();
+        let tz_minutes: i32 = caps.at(3).unwrap().parse().unwrap();
+
+        let nd = NaiveDateTime::from_timestamp(epoch, 0);
+        let tz = FixedOffset::west((tz_hours * 3600) + (tz_minutes * 60));
+        let fotz: DateTime<FixedOffset> = DateTime::from_utc(nd, tz);
+        fotz.with_timezone(&Local)
+    }
+}
+
+#[allow(non_snake_case)]
+#[derive(RustcDecodable, RustcEncodable, Debug)]
+struct TerminalCombo {
+    Times: Vec<SailingTime>,
+    DepartingTerminalName: String,
+    ArrivingTerminalName: String,
+}
+
+#[allow(non_snake_case)]
+#[derive(RustcDecodable, RustcEncodable, Debug)]
 struct ScheduleResult {
     TerminalCombos: Vec<TerminalCombo>,
 }
 
-fn main() {
-    let mut args = env::args();
-    let _program = args.next().unwrap();
-    let route: &str = &args.next()
-                           .expect("must pass one of sea-bi or bi-sea");
-
-    let (from, to) = match route {
-        "sea-bi" => (7,3),
-        "bi-sea" => (3, 7),
-        _ => panic!("only bi-sea and sea-bi supported"),
-    };
-
-    let now = Local::now();
-    let s = Session::new(env!("WSDOT_API_KEY"));
-    let url = &s.url(format!("/schedule/{}-{}-{}/{}/{}",
-                             now.year(), now.month(), now.day(),
-                             from, to));
-    //println!("{}", url);
-
-    let client = Client::new();
-    let mut res = client.get(url).send().unwrap();
-
-    assert_eq!(res.status, hyper::Ok);
-    let mut s = String::new();
-    res.read_to_string(&mut s).unwrap();
-    let routes: ScheduleResult = json::decode(&s).unwrap();
-
-    assert_eq!(1, routes.TerminalCombos.len());
-    for time in routes.TerminalCombos[0].Times.iter() {
-        let dt = time.depart_time(now.timezone());
-        if dt > now {
-            println!("{}", dt.time());
-        }
-    }
-}
-
 #[test]
-fn test_regex() {
-    // parse strings of form "/Date(1436318400000-0700)/"
-    let re = Regex::new(r"^/Date\((\d{10})000-(\d{4})\)/$").unwrap();
-    let caps = re.captures("/Date(1436318400000-0700)/").unwrap();
-    assert_eq!(caps.at(1).unwrap(), "1436318400");
-    assert_eq!(caps.at(2).unwrap(), "0700");
+fn test_hashmap() {
+    let mut h: HashMap<&str, i32> = HashMap::new();
+    h.insert("bi", 3);
+    h.insert("sea", 7);
+    match h.get("bi") {
+        Some(id) => assert_eq!(*id, 3),
+        None => assert!(false)
+    }
 }
