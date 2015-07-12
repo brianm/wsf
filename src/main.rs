40ca72d055551a440b67a95f7651c08b278858ff
Brian McCallister <brianm@skife.org>
2015-07-12 15:22:04 -0700

    mucking about cleaning up my lifetime understandings

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ extern crate hyper;
 extern crate rustc_serialize;
 extern crate chrono;
 extern crate regex;
+extern crate env_logger;
+#[macro_use] extern crate log;
 
 use std::ascii::AsciiExt;
 use std::io::Read;
 use std::env;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
 
 use chrono::naive::datetime::NaiveDateTime;
 use chrono::offset::local::Local;
@@ -18,19 +24,20 @@ use hyper::client::Client;
 use regex::Regex;
 
 fn main() {
+    env_logger::init().unwrap();
+
     let mut args      = env::args();
     let _program      = args.next().expect("program should be first arg!");
     let from_in: &str = &args.next().expect("must specify from");
     let to_in: &str   = &args.next().expect("must specify to");
 
     // pull in api key at *build* time from environment
-    let s = Session::new(env!("WSDOT_API_KEY"));
+    let mut s = Session::new(env!("WSDOT_API_KEY"));
 
     let now = Local::now();
     let mut from: Option<i32> = None;
     let mut to: Option<i32> = None;
-    let terminals = s.terminals();
-    for terminal in terminals.iter() {
+    for terminal in s.terminals().iter() {
         if terminal.Description.to_ascii_lowercase().starts_with(&from_in) {
             from = Some(terminal.TerminalID);
         }
@@ -39,18 +46,7 @@ fn main() {
         }
     }
 
-    let url = &s.url(format!("/schedule/{}-{}-{}/{}/{}",
-                             now.year(), now.month(), now.day(), from.unwrap(), to.unwrap()));
-
-    let mut res = s.client.get(url).send().unwrap();
-    assert_eq!(res.status, hyper::Ok);
-
-    let mut buf = String::new();
-    res.read_to_string(&mut buf).unwrap();
-    let schedule: ScheduleResult = json::decode(&buf).unwrap();
-
-    assert_eq!(1, schedule.TerminalCombos.len());
-    let tc = &schedule.TerminalCombos[0];
+    let tc = s.schedule(from.unwrap(), to.unwrap());
     for time in tc.Times.iter() {
         if time.depart_time() > now {
             println!("{}\t{}\t{}\t{}",
@@ -61,19 +57,44 @@ fn main() {
         }
 
     }
+    s.save_cache();
 }
 
 struct Session {
     api_key: String,
     client: Client,
+    cache: Cache,
+    cacheflushdate: String,
+    cache_path: String,
 }
 
 impl Session {
+
     fn new(api_key: &str) -> Session {
-        Session {
+        let mut cache_path: PathBuf = env::home_dir().unwrap();
+        cache_path.push(".wsf.cache");
+        let cache_path = format!("{}", cache_path.display());
+
+        let mut s = Session {
             api_key: api_key.to_string(),
             client: Client::new(),
-        }
+            cache: Cache::load(&cache_path),
+            cacheflushdate: String::new(),
+            cache_path: cache_path,
+        };
+
+        let url = &s.url(format!("/cacheflushdate"));
+        let mut r = s.client.get(url).send().unwrap();
+        assert_eq!(r.status, hyper::Ok);
+        r.read_to_string(&mut s.cacheflushdate).unwrap();
+        s
+    }
+
+    fn save_cache(&mut self) {
+        self.cache.cache_flush_date = self.cacheflushdate.clone();
+        let mut f = File::create(&self.cache_path).unwrap();
+        let encoded = json::encode(&self.cache).unwrap();
+        f.write_all(encoded.as_bytes()).unwrap();
     }
 
     fn url(&self, path: String) -> String {
@@ -82,17 +103,84 @@ impl Session {
                 self.api_key)
     }
 
-    fn terminals(&self) -> Vec<Terminal> {
-        let now = Local::today();
-        let url = &self.url(format!("/terminals/{}-{}-{}", now.year(), now.month(), now.day()));
+    fn terminals(&mut self) -> Vec<Terminal> {
+        if self.cache.cache_flush_date == self.cacheflushdate {
+            self.cache.terminals.clone()
+        }
+        else {
+            let now = Local::today();
+            let url = &self.url(format!("/terminals/{}-{}-{}", now.year(), now.month(), now.day()));
+
+            let mut res = self.client.get(url).send().unwrap();
+            assert_eq!(res.status, hyper::Ok);
+
+            let mut buf = String::new();
+            res.read_to_string(&mut buf).unwrap();
+            let routes: Vec<Terminal> = json::decode(&buf).unwrap();
+            self.cache.terminals = routes.clone();
+            routes
+        }
+    }
+
+    fn schedule(&mut self, from: i32, to: i32) -> TerminalCombo {
+        let mut cache_is_stale = true;
+        let cache_key = format!("{} {}", from, to);
+
+        if self.cache.cache_flush_date == self.cacheflushdate {
+            if self.cache.sailings.contains_key(&cache_key) {
+                // cache is up to date and has route!
+                return self.cache.sailings.get(&cache_key).unwrap().clone()
+            }
+            else {
+                // cache is up to date, but we don't have this route in it
+                cache_is_stale = false;
+            }
+        }
+
+        if cache_is_stale {
+            self.cache.sailings.clear();
+        }
+
+        let now = Local::now();
+        let url = &self.url(format!("/schedule/{}-{}-{}/{}/{}",
+                                    now.year(), now.month(), now.day(), from, to));
 
         let mut res = self.client.get(url).send().unwrap();
         assert_eq!(res.status, hyper::Ok);
 
         let mut buf = String::new();
         res.read_to_string(&mut buf).unwrap();
-        let routes: Vec<Terminal> = json::decode(&buf).unwrap();
-        routes
+        let schedule: ScheduleResult = json::decode(&buf).unwrap();
+        self.cache.sailings.insert(cache_key, schedule.TerminalCombos[0].clone());
+        schedule.TerminalCombos[0].clone()
+    }
+}
+
+#[derive(RustcDecodable, RustcEncodable, Debug)]
+struct Cache {
+    terminals: Vec<Terminal>,
+    sailings: HashMap<String, TerminalCombo>,
+    cache_flush_date: String,
+}
+
+impl Cache {
+    fn load(path: &String) -> Cache {
+        let r = File::open(path);
+        match r {
+            Ok(mut f) => {
+                let mut s = String::new();
+                f.read_to_string(&mut s).unwrap();
+                let cache = json::decode(&s).unwrap();
+                cache
+            },
+            Err(_) => {
+                Cache {
+                    terminals: vec![],
+                    sailings: HashMap::new(),
+                    cache_flush_date: String::new(),
+                }
+            }
+        }
     }
 }
 
@@ -104,7 +192,7 @@ struct Terminal {
 }
 
 #[allow(non_snake_case)]
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcDecodable, RustcEncodable, Clone, Debug)]
 struct SailingTime {
     DepartingTime: String,
     ArrivingTime: Option<String>,
@@ -129,7 +217,7 @@ impl SailingTime {
 }
 
 #[allow(non_snake_case)]
-#[derive(RustcDecodable, RustcEncodable, Debug)]
+#[derive(RustcDecodable, RustcEncodable, Clone, Debug)]
 struct TerminalCombo {
     Times: Vec<SailingTime>,
     DepartingTerminalName: String,
