dc0aff672ab3e4089e7f2fe73f4886f3c6893a70
Brian McCallister <brianm@skife.org>
2019-03-09 12:10:08 -0800

    split to main/lib and update some libs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,10 @@
-use hyper;
-use rustc_serialize;
+#[macro_use]
+extern crate serde_derive;
 
-use dirs;
-use env_logger;
-use log;
-
-use std::collections::HashMap;
-use std::error::Error;
-use std::fmt;
-use std::fs::File;
-use std::io::Read;
-use std::io::Write;
-use std::path::PathBuf;
-use std::result;
-
-use chrono::datetime::DateTime;
-use chrono::naive::datetime::NaiveDateTime;
-use chrono::offset::fixed::FixedOffset;
 use chrono::offset::local::Local;
-use chrono::Datelike;
-
-use hyper::client::Client;
-use rustc_serialize::json;
-use rustc_serialize::Decodable;
-
-use regex::Regex;
-
 use docopt::Docopt;
+use env_logger;
+use wsf;
 
 static USAGE: &'static str = "
 Washing State Ferry Schedules
@@ -44,41 +22,38 @@ Options:
   -h --help     Show this screen.
 ";
 
-#[derive(Debug, RustcDecodable)]
+#[derive(Debug, Deserialize)]
 struct Args {
     arg_from: String,
     arg_to: String,
     flag_all: bool,
 }
 
-fn run() -> Result<()> {
-    env_logger::init()?;
+fn run() -> Result<(), wsf::CliError> {
+    env_logger::init();
 
     let args: Args = Docopt::new(USAGE)
-        .and_then(|d| d.decode())
+        .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
     let from_in: &str = &args.arg_from.to_ascii_lowercase();
     let to_in: &str = &args.arg_to.to_ascii_lowercase();
 
-    let mut s = Session::new("afddf683-37c5-4d1a-8486-f7004a16d86d");
+    let mut s = wsf::Session::new("afddf683-37c5-4d1a-8486-f7004a16d86d");
 
     let now = Local::now();
     let from = s
         .terminals()?
         .iter()
         .find(|t| t.Description.to_ascii_lowercase().starts_with(&from_in))
-        .ok_or(CliError::BadInput(format!(
-            "From port, '{}', is not known!",
-            from_in
-        )))?
+        .ok_or_else(|| wsf::CliError::BadInput(format!("From port, '{}', is not known!", from_in)))?
         .TerminalID;
 
     let to = s
         .terminals()?
         .iter()
         .find(|t| t.Description.to_ascii_lowercase().starts_with(&to_in))
-        .ok_or(CliError::BadInput(format!(
+        .ok_or_else(|| wsf::CliError::BadInput(format!(
             "To port, '{}', is not known !",
             from_in
         )))?
@@ -95,16 +70,14 @@ fn run() -> Result<()> {
                 tc.ArrivingTerminalName,
                 time.VesselName
             );
-        } else {
-            if time.depart_time() > now {
-                println!(
-                    "{}\t{}\t{}\t{}",
-                    time.depart_time().time(),
-                    tc.DepartingTerminalName,
-                    tc.ArrivingTerminalName,
-                    time.VesselName
-                );
-            }
+        } else if time.depart_time() > now {
+            println!(
+                "{}\t{}\t{}\t{}",
+                time.depart_time().time(),
+                tc.DepartingTerminalName,
+                tc.ArrivingTerminalName,
+                time.VesselName
+            );
         }
     }
     s.save_cache()
@@ -117,250 +90,5 @@ fn main() {
             eprintln!("{}", e);
             std::process::exit(1);
         }
-    }
-}
-
-struct Session {
-    api_key: String,
-    client: Client,
-    cache: Cache,
-    cacheflushdate: String,
-    cache_path: String,
-    offline: bool,
-}
-
-impl Session {
-    fn new(api_key: &str) -> Session {
-        let mut cache_path: PathBuf = dirs::home_dir().unwrap();
-        cache_path.push(".wsf.cache");
-        let cache_path = cache_path.display().to_string();
-
-        let mut s = Session {
-            api_key: api_key.to_string(),
-            client: Client::new(),
-            cache: Cache::load(&cache_path).unwrap_or(Cache::empty()),
-            cacheflushdate: String::new(),
-            cache_path,
-            offline: false,
-        };
-
-        // TODO this is kind of gross, cfd as optional to indicate offline, maybe?
-        s.offline = match s.get::<String>("/cacheflushdate".to_owned()) {
-            Ok(cfd) => {
-                s.cacheflushdate = cfd;
-                false
-            }
-            Err(_) => true,
-        };
-        return s;
-    }
-
-    fn save_cache(&mut self) -> Result<()> {
-        self.cache.cache_flush_date = self.cacheflushdate.clone();
-
-        let mut f = File::create(&self.cache_path)?;
-        let encoded = json::encode(&self.cache)?;
-        Ok(f.write_all(encoded.as_bytes())?)
-    }
-
-    fn get<T: Decodable>(&self, path: String) -> Result<T> {
-        let url = &format!(
-            "http://www.wsdot.wa.gov/ferries/api/schedule/rest{}?apiaccesscode={}",
-            path, self.api_key
-        );
-        let mut res = self.client.get(url).send()?;
-        assert_eq!(res.status, hyper::Ok);
-
-        let mut buf = String::new();
-        res.read_to_string(&mut buf)?;
-        Ok(json::decode::<T>(&buf)?)
-    }
-
-    fn terminals(&mut self) -> Result<Vec<Terminal>> {
-        if self.offline || (self.cache.cache_flush_date == self.cacheflushdate) {
-            return Ok(self.cache.terminals.clone());
-        } else {
-            let now = Local::today();
-            let path = format!("/terminals/{}-{}-{}", now.year(), now.month(), now.day());
-            let routes: Vec<Terminal> = self.get(path)?;
-            self.cache.terminals = routes.clone();
-            return Ok(routes);
-        }
-    }
-
-    fn schedule(&mut self, from: i32, to: i32) -> Result<TerminalCombo> {
-        let mut cache_is_stale = true;
-        let cache_key = format!("{} {}", from, to);
-
-        if self.offline || (self.cache.cache_flush_date == self.cacheflushdate) {
-            if self.cache.sailings.contains_key(&cache_key) {
-                // cache is up to date and has route!
-                return Ok(self
-                    .cache
-                    .sailings
-                    .get(&cache_key)
-                    .expect("checked for key in cache then not found")
-                    .clone());
-            } else {
-                // cache is up to date, but we don't have this route in it
-                cache_is_stale = false;
-            }
-        }
-
-        if cache_is_stale {
-            self.cache.sailings.clear();
-        }
-
-        let now = Local::now();
-        let path = format!(
-            "/schedule/{}-{}-{}/{}/{}",
-            now.year(),
-            now.month(),
-            now.day(),
-            from,
-            to
-        );
-
-        let schedule: Schedule = self.get(path)?;
-
-        self.cache
-            .sailings
-            .insert(cache_key, schedule.TerminalCombos[0].clone());
-        Ok(schedule.TerminalCombos[0].clone())
-    }
-}
-
-#[derive(RustcDecodable, RustcEncodable, Debug)]
-struct Cache {
-    terminals: Vec<Terminal>,
-    sailings: HashMap<String, TerminalCombo>,
-    cache_flush_date: String,
-}
-
-impl Cache {
-    fn empty() -> Cache {
-        Cache {
-            terminals: vec![],
-            sailings: HashMap::new(),
-            cache_flush_date: String::new(),
-        }
-    }
-
-    fn load(path: &String) -> Result<Cache> {
-        let mut f = File::open(path)?;
-        let mut s = String::new();
-        f.read_to_string(&mut s)?;
-        let cache: Cache = json::decode(&s)?;
-        Ok(cache)
-    }
-}
-
-#[allow(non_snake_case)]
-#[derive(RustcDecodable, RustcEncodable, Debug, Clone)]
-struct Terminal {
-    TerminalID: i32,
-    Description: String,
-}
-
-#[allow(non_snake_case)]
-#[derive(RustcDecodable, RustcEncodable, Clone, Debug)]
-struct SailingTime {
-    DepartingTime: String,
-    ArrivingTime: Option<String>,
-    VesselName: String,
-}
-
-impl SailingTime {
-    // parse date strings of form "/Date(1436318400000-0700)/"
-    fn depart_time(&self) -> DateTime<Local> {
-        let re = Regex::new(r"^/Date\((\d{10})000-(\d{2})(\d{2})\)/$").unwrap();
-        let caps = re.captures(&self.DepartingTime).unwrap();
-
-        let epoch: i64 = caps.at(1).unwrap().parse().unwrap();
-        let tz_hours: i32 = caps.at(2).unwrap().parse().unwrap();
-        let tz_minutes: i32 = caps.at(3).unwrap().parse().unwrap();
-
-        let nd = NaiveDateTime::from_timestamp(epoch, 0);
-        let tz = FixedOffset::west((tz_hours * 3600) + (tz_minutes * 60));
-        let fotz: DateTime<FixedOffset> = DateTime::from_utc(nd, tz);
-        fotz.with_timezone(&Local)
-    }
-}
-
-#[allow(non_snake_case)]
-#[derive(RustcDecodable, RustcEncodable, Clone, Debug)]
-struct TerminalCombo {
-    Times: Vec<SailingTime>,
-    DepartingTerminalName: String,
-    ArrivingTerminalName: String,
-}
-
-#[allow(non_snake_case)]
-#[derive(RustcDecodable, RustcEncodable, Debug)]
-struct Schedule {
-    TerminalCombos: Vec<TerminalCombo>,
-}
-
-type Result<T> = result::Result<T, CliError>;
-
-#[derive(Debug)]
-enum CliError {
-    Log(log::SetLoggerError),
-    Parse(rustc_serialize::json::DecoderError),
-    SaveCache(rustc_serialize::json::EncoderError),
-    Http(hyper::error::Error),
-    Io(std::io::Error),
-    BadInput(String),
-}
-
-impl fmt::Display for CliError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            CliError::Log(ref err) => format!("Unable to configure logging: {}", err).fmt(f),
-            CliError::Parse(ref err) => format!("Unable to parse WSDOT Data: {}", err).fmt(f),
-            CliError::SaveCache(ref err) => format!("Unable to save cache: {}", err).fmt(f),
-            CliError::Http(ref err) => format!("Unable to communicate with WSDOT: {}", err).fmt(f),
-            CliError::Io(ref err) => format!("Unable to read data: {}", err).fmt(f),
-            CliError::BadInput(ref desc) => format!("Unable to understand input: {}", desc).fmt(f),
-        }
-    }
-}
-
-impl Error for CliError {
-    fn description(&self) -> &str {
-        match *self {
-            CliError::BadInput(ref err) => err,
-            _ => self.description(),
-        }
-    }
-}
-
-impl From<rustc_serialize::json::EncoderError> for CliError {
-    fn from(err: rustc_serialize::json::EncoderError) -> CliError {
-        CliError::SaveCache(err)
-    }
-}
-
-impl From<log::SetLoggerError> for CliError {
-    fn from(err: log::SetLoggerError) -> CliError {
-        CliError::Log(err)
-    }
-}
-
-impl From<hyper::error::Error> for CliError {
-    fn from(err: hyper::error::Error) -> CliError {
-        CliError::Http(err)
-    }
-}
-
-impl From<std::io::Error> for CliError {
-    fn from(err: std::io::Error) -> CliError {
-        CliError::Io(err)
-    }
-}
-
-impl From<rustc_serialize::json::DecoderError> for CliError {
-    fn from(err: rustc_serialize::json::DecoderError) -> CliError {
-        CliError::Parse(err)
     }
 }
