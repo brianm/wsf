b5ba081d37601450ca0ce1f75f83ce350c894d32
Brian McCallister <brianm@skife.org>
2022-04-22 20:29:49 -0700

    * Remove pandoc from normal builds (check in the manpage) * update various dependencies * add makefile which knows how to regen the manpage

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+.PHONY: build
+build: 
+	@echo "use cargo to build, this is here only for the manpage (make man)"
+
+.PHONY: man
+man:
+	pandoc wsf.1.md -s -t man -o wsf.1
--- build.rs
+++
@@ -1,8 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    Command::new("pandoc")
-        .args(&["wsf.1.md", "-s", "-t", "man", "-o", "wsf.1"])
-        .status()
-        .unwrap();
-}
